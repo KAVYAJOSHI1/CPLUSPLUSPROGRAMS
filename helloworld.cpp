8dfe81f3433a1afb0062db0dc83aba259783b6c6
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-06-18 16:49:01 +0530

    commit

---
+++ helloworld.cpp
@@ -0,0 +1,9 @@
+//PROGRAM TO PRINT HELLO WORLD
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello World!" ;
+    return 0;
+}
---
+++ sum.cpp
@@ -0,0 +1,12 @@
+//SUM OF TWO NUMBERS
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b;
+    cout << "Enter two numbers:\n ";
+    cin >> a ;
+    cin>> b;
+    cout << "Sum of two numbers is: " << a + b;
+    return 0;
+}

8114c7310bacaada504cec24d3576364daabbc5f
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-06-19 20:06:04 +0530

    commit

--- armstrongnumber.cpp
+++ armstrongnumber.cpp
@@ -1,26 +1,37 @@
-//153=1+125+27
+//PROGRAM TO FIND AN ARMSTRONG NUMBER
 #include <iostream>
+#include <math.h>
 using namespace std;
+
 int main()
 {
     int n;
-    cout<<"ENTER THE NUMBER:\n";
-    cin>>n;
-    int temp=n;
-    int sum=0;
-    while(temp!=0)
+    cout <<"ENTER THE NUMBER:\n";
+    cin >>n;
+    int n1=n;
+    int n2=n;
+    int count=0;
+    while(n1>0)
     {
-        int rem=temp%10;
-        sum=sum+rem*rem*rem;
-        temp=temp/10;
-    }
-    if(sum==n)
-    {
-        cout<<"THE NUMBER IS ARMSTRONG";
-    }
-    else
-    {
-        cout<<"THE NUMBER IS NOT ARMSTRONG";
-    }
+        int rem=n1%10;
+        count++;
+        n1=n1/10;
 
+    }
+    int sum=0;
+    while(n2>0)
+    {
+        int rem=n2%10;
+        int a=pow(rem,count);
+        sum=sum+a;
+        n2=n2/10;
+    }
+    cout<<sum;
+    if(n==sum)
+    {
+        cout<<"ARMSTRONG NUMBER\n";
+    }
+    else{
+        cout<<"NOT ARMSTRONG NUMBER\n";
+    }
 }

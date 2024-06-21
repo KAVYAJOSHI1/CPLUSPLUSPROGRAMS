73417272ec00d2bcded7b298437d51c9f3c03401
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-06-21 16:35:34 +0530

    COMMIT

---
+++ leapyear.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"ENTER NUMBER";
+    cin>>n;
+    if(n%4==0)
+    {
+        cout<<"IT IS A LEAP YEAR";
+    }
+    else
+    {
+        cout<<"IT IS NOT A LEAP YEAR";
+    }
+
+}

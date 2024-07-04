5ba3c9bd6f45f473e562118a5d67d6e6d83398a7
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-07-04 10:57:55 +0530

    commit

---
+++ complex.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+class COMPLEX
+{
+    private:
+    int real,imag;
+    public:
+    void read()
+    {
+        cout<<"ENTER THE REAL AND IMAGINARY PARTS"<<endl;
+        cin>>real>>imag;
+    }
+    void add(COMPLEX c1,COMPLEX c2)
+    {
+        real=c1.real+c2.real;
+        imag=c1.imag+c2.imag;
+    }
+    void display() 
+    {
+        cout<<"THE SUM IS "<<real<<"+"<<imag<<"i"<<endl;
+    }     
+    };
+
+int main()
+{
+COMPLEX c1,c2,c3;
+c1.read();
+c2.read();
+c3.add(c1,c2);
+c3.display();
+}

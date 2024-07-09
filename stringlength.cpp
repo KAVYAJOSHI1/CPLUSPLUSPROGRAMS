959f9c89191cef5530d043a9b499d251ded5eb59
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-07-09 23:32:15 +0530

    A

--- stringlength.cpp
+++ stringlength.cpp
@@ -5,6 +5,10 @@ int main()
     cout << "Enter the string: ";
     string str;
     getline(cin,str);
+    
+    str.push_back('J');
+    str.append("OSHI");
     int x=str.length();
+    cout << "STRING AFTER APPENDING IS: "<<str;
     cout << "LENGTH OF THE STRING IS:"<<x;
 }

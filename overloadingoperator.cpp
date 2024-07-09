7a9b6f981ebdcde4eb90ba6e45aecd6487b798a3
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-07-09 15:23:00 +0530

    commit

---
+++ overloadingoperator.cpp
@@ -0,0 +1,27 @@
+//overloading unary operator -
+
+#include <iostream>
+using namespace std;
+class space{
+    int a;
+    int b;
+    public:
+    void getdata(int x,int y){
+        a=x;
+        b=y;
+    }
+    void operator -(){
+        a=-a;
+        b=-b;
+    }
+    void display(){
+        cout<<a<<b<<endl;
+}
+};
+int main()
+{
+    space s1;
+    s1.getdata(10,20);
+    -s1;
+    s1.display();
+}

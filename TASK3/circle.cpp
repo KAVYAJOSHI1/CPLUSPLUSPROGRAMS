3679c3798157e205e66948a68e83f0bff461e9c9
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-07-14 22:31:10 +0530

    a

---
+++ TASK3/circle.cpp
@@ -0,0 +1,26 @@
+/*5. Write a program to create a class Circle with a data member radius. Include an inline
+member function to calculate and return the area of the circle. Create an object to find and
+display the area of the circle*/
+#include <iostream>
+using namespace std;
+class Circle
+{
+    int radius;
+
+public:
+ Circle(int r)
+{
+    radius = r;
+
+}
+int inline area()
+{
+    return (3.14 * radius * radius);
+}
+};
+int main()
+{
+    Circle c(5);
+    cout << "Area of the circle is: " << c.area() << endl;
+    return 0;
+}

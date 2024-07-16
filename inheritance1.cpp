eda4f40beab48ae83c6f0b34a6249db5b6091f56
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-07-16 15:24:06 +0530

    a

--- inheritance1.cpp
+++ inheritance1.cpp
@@ -1,17 +1,28 @@
 #include <iostream>
-using namespace std
+using namespace std;
 class a
-
 {
     public:
-    int legs=4
+    int legs=4;
     void display()
     {
-        cout<<legs
+        cout<<legs;
     }
 
 };
 class b: public a
 {
-
+    bool tail=true;
+    public:
+    void display2()
+    {
+        cout<<tail;
+    }
+};
+int main()
+{
+    a a1;
+    b b1;
+    b1.display1();
+    b1.display2();
 }

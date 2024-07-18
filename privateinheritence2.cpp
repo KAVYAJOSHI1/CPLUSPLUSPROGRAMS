f6ca758b61e07d6b94f0696f163f69a51d068b6a
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-07-18 13:29:25 +0530

    a

---
+++ privateinheritence2.cpp
@@ -0,0 +1,32 @@
+//to acces private member of class a in b
+//Private members can be accessed through functions
+
+#include <iostream>
+using namespace std;
+class A{
+    private:
+    int a;
+    public:
+    int b;
+    void set(int x,int y){
+        a=x;
+        b=y;
+    }
+    int get(){
+        return a;
+        
+    }
+};
+class B:public A{
+    public:
+    void display(){
+        
+        cout<<b*get();
+    }
+};
+int main(){
+    B obj;
+    obj.set(10,20);
+    obj.display();
+    return 0;
+}

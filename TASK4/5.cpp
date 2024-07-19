f31bac21d57e05cfb024c484ae35518259f11746
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-07-19 17:13:11 +0530

    a

---
+++ TASK4/5.cpp
@@ -0,0 +1,4 @@
+/*Write a program to create class Time with members hour, minutes and seconds. Overload
+the binary operator plus(+) where two objects of time are added using (i) operator
+overloading and (ii) Friend function . Write a display() function to print valid time in 12
+hours format.*/

5bb3810ce899b639b912769c36fbc3dad1a49c27
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-07-19 20:24:28 +0530

    a

---
+++ vector1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+ int main()
+ {
+  vector<int> v;
+  v.assign(5,10);
+  for(int i=0;i<v.size();i++)
+  {
+   cout<<v[i]<<endl;
+  }
+ }

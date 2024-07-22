a0dd0e20ef13b622c8628a0a92d7e5ede6aa8a3c
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-07-22 23:28:57 +0530

    a

--- vector3.cpp
+++ vector3.cpp
@@ -12,5 +12,14 @@ int main()
     cout<<"Vector size : "<<v.size()<<endl;
     cout<<"Vector capacity : "<<v.capacity()<<endl;
     cout<<"Vector max_size : "<<v.max_size()<<endl;
-    
+    v.resize(5);
+    cout<<"Vector size : "<<v.size()<<endl;
+    if(v.empty()==false)
+    {
+        cout<<"Vector is not empty"<<endl;
+    }
+    else
+    {
+        cout<<"Vector is empty"<<endl;
+    }
     }

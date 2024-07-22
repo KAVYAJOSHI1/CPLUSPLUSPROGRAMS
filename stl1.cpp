f1761dd9d7f6bee59284df4cca149496841eb215
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-07-22 23:42:44 +0530

    a

--- stl1.cpp
+++ stl1.cpp
@@ -7,7 +7,24 @@ int main()
 
     for(int i=1;i<=10;i++)
     {
-        v.pushback(i);
+        v.push_back(i);
     }
     cout<<"Size of vector is :"<<v.size()<<endl;
+    v.resize(7);
+    cout<<"After resize, size of vector is :"<<v.size()<<endl;
+    if(v.empty()==false)
+    {
+        cout<<"Vector is not empty"<<endl;
+    }
+    else
+    {
+        cout<<"Vector is empty"<<endl;
+    }
+    cout<<"ELEMENTS OF VECTOR ARE :"<<endl;
+    for(auto it=v.begin();it!=v.end();it++)
+    {
+        cout<<*it<<endl;
+    }
+
+    return 0;
 }

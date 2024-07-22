dd07597dedd623426a18de23624091d1a7f616b3
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-07-22 23:14:01 +0530

    a

--- vector1.cpp
+++ vector1.cpp
@@ -9,4 +9,18 @@ using namespace std;
   {
    cout<<v[i]<<endl;
   }
+v.push_back(20);
+  for(int i=0;i<v.size();i++)
+  {
+   cout<<v[i]<<endl;
+  }
+  v.pop_back();
+  cout<<"Now the contents are:";
+  for(int i=0;i<v.size();i++)
+  {
+   cout<<v[i]<<endl;
+  }
+  
+
+
  }

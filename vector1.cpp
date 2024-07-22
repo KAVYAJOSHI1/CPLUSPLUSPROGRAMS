2bf47103a0424a2cbb9efb9c4ef701f7e9419e0a
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-07-22 23:14:49 +0530

    a

--- vector1.cpp
+++ vector1.cpp
@@ -20,7 +20,20 @@ v.push_back(20);
   {
    cout<<v[i]<<endl;
   }
-  
+  v.insert(v.begin()+1,30);
+  cout<<"Now the contents are:";
+  for(int i=0;i<v.size();i++)
+  {
+   cout<<v[i]<<endl;
+  }
+  v.erase(v.begin()+1);
+  cout<<"Now the contents are:";
+  for(int i=0;i<v.size();i++)
+  {
+   cout<<v[i]<<endl;
+  }
+  return 0;
+
 
 
  }

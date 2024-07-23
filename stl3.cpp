0d8adcdbc8084844fa4c058eea1e8970101aa3a8
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-07-23 22:32:47 +0530

    a

---
+++ stl3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i=7;
+    int j=10;
+    cout<<"Vlaue of i is :"<<i<<endl;
+    cout<<"Value of j is :"<<j<<endl;
+    swap(i,j);
+    cout<<"After swapping i is :"<<i<<endl;
+    cout<<"After swapping j is :"<<j<<endl;
+    return 0;
+}

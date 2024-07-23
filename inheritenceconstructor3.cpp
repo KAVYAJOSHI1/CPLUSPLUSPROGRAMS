ee0ace25e51275b0881c7b6dd4e4b4dabc665c78
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-07-23 15:30:53 +0530

    a

---
+++ inheritenceconstructor3.cpp
@@ -0,0 +1,38 @@
+//using multilevel inheritance
+#include <iostream>
+using namespace std;
+class A
+{
+    public:
+    int a
+    A(int x)
+    {
+        a=x;
+        cout<<a;
+    }
+};
+class B:public A
+{
+    public:
+    int b
+    B(int x,int y):A(x)
+    {
+        b=y;
+        cout<<b;
+    }
+};
+class C:public B
+{
+    public:
+    int c
+    C(int x,int y,int z):B(x,y)
+    {
+        c=z;
+        cout<<c;
+    }
+
+}
+int main()
+{
+    C c(1,2,3);
+}

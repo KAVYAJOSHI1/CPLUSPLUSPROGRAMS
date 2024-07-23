b3ed63f25ede6ae7c6431f76d6471924d6250fd2
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-07-23 22:50:09 +0530

    a

---
+++ enum2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+enum Supercars{Venom=301 ,Veryon=268,Valkyrie=240,Roadster=250};
+int main()
+{
+    Supercars speed1;
+    Supercars speed2;
+    speed1=Valkyrie;
+    speed2=Roadster;
+
+    if(speed1<speed2)
+    {
+        cout<<"Valkyrie is faster than Roadster"<<endl;
+    }
+}

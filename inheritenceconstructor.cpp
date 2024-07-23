32778122067d382b00ddf0e5ea8e8ae5867f62ed
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-07-23 15:20:53 +0530

    a

--- inheritenceconstructor.cpp
+++ inheritenceconstructor.cpp
@@ -1,1 +1,27 @@
 #include <iosteam>
+using namespace std;
+class base
+{
+    int x;
+    base(int i)
+    {
+        x=i;
+        cout<<"base class constructor"<<endl;
+        cout<<x;
+    }
+};
+class Derived:public base
+{
+    int y;
+    Derived(int i,int j):base(i)
+    {
+        y=j;
+        cout<<"Derived class constructor "<<endl;
+        cout<<y;
+    }
+};
+int main()
+{
+    Derived ob(10,20);
+    return 0;
+}

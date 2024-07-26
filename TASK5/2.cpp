ea3d39937276136e09d2556f79bd02e4f5ec3ee9
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-07-26 17:21:41 +0530

    a

--- TASK5/2.cpp
+++ TASK5/2.cpp
@@ -6,24 +6,90 @@ Result from Test with total and percentage as members and calculate() function
 as member function to calculate percentage of student. In main create the
 object and display all the student details.*/
 
-#include<iostream>
+#include <iostream>
+#include <string>
 using namespace std;
-class Student{
-    public:
+
+class Student
+{
+public:
     int enrol_no;
     string name;
-    int class;
+    string student_class;
+
     void getdata()
     {
-        cout<<"ENTER THE NAME OF THE STUDENT"<<endl;
-        cin>>name;
-        cout<<"ENTER THE ROLL NUMBER OF THE STUDENT"<<endl;
-        cin>>enrol_no;
-        cout<<"ENTER THE CLASS OF THE STUDENT"<<endl;
-        cin>>class;
+        cout << "Enter enrollment number: ";
+        cin >> enrol_no;
+        cout << "Enter name: ";
+        cin >> name;
+        cout << "Enter class: ";
+        cin >> student_class;
+    }
+
+    void display()
+    {
+        cout << "Enrollment Number: " << enrol_no << endl;
+        cout << "Name: " << name << endl;
+        cout << "Class: " << student_class << endl;
     }
 };
 
-class Test:public Student
+class Test : public Student
 {
-    public:
+public:
+    float marks[5];
+
+    void getdata()
+    {
+        Student::getdata();
+        for (int i = 0; i < 5; ++i)
+        {
+            cout << "Enter marks for subject " << i + 1 << ": ";
+            cin >> marks[i];
+        }
+    }
+
+    void display()
+    {
+        Student::display();
+        for (int i = 0; i < 5; ++i)
+        {
+            cout << "Marks for subject " << i + 1 << ": " << marks[i] << endl;
+        }
+    }
+};
+
+class Result : public Test
+{
+public:
+    float total;
+    float percentage;
+
+    void calculate()
+    {
+        total = 0;
+        for (int i = 0; i < 5; ++i)
+        {
+            total += marks[i];
+        }
+        percentage = total / 5;
+    }
+
+    void display()
+    {
+        Test::display();
+        cout << "Total Marks: " << total << endl;
+        cout << "Percentage: " << percentage << "%" << endl;
+    }
+};
+
+int main()
+{
+    Result r1;
+    r1.getdata();
+    r1.calculate();
+    r1.display();
+
+    return 0;
+}

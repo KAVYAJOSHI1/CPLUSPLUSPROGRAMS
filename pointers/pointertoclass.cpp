b1ad6af3c957640effb9e5ca5e1244fb26a23ad0
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-07-26 15:23:26 +0530

    a

---
+++ pointers/pointertoclass.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class demo(int x)
+{
+    int i;
+    public:
+    demo(int x)
+    {
+        i=x;
+    }
+    void display()
+    {
+        cout<<"i="<<i;
+    }
+};
+int main()
+{
+    demo d1(10);
+    demo *ptr=&d1;
+    ptr->display();
+}

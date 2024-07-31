da38f743c4c596e5785aabb7b81e22c41aef5a48
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-07-31 23:45:00 +0530

    a

---
+++ basictoclasstypecoversion.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+class a
+{
+    public :
+    int n;
+    a(){}
+
+  a (int x)
+  {
+n=x;
+cout<<n;
+  }
+};
+int main()
+{
+    a obj;
+    int m=10;
+    obj=m;
+
+}

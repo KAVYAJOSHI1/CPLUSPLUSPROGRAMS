58e1e3f52e1bfc21914a44b62766eefe5b5d1c8f
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-08-06 23:09:45 +0530

    a

--- arraywithobject.cpp
+++ arraywithobject.cpp
@@ -1,1 +1,73 @@
-//TO USE ARRAY IN OBJECTS
+//TO USE ARRAY IN OBJECTS sum of 2 arrays using object
+
+#include<iostream>
+using namespace std;
+
+class Array
+{
+
+    int arr[3];
+    public:
+
+    Array()
+    {
+        for(int i=0;i<3;i++)
+        {
+            arr[i]=0;
+        }
+    }
+
+    Array operator+(Array obj)
+    {
+        Array temp;
+        for(int i=0;i<3;i++)
+        {
+            temp.arr[i]=arr[i]+obj.arr[i];
+        }
+        return temp;
+    }
+
+    void setvalue()
+    {
+        for(int i=0;i<3;i++)
+        {
+            cin>>arr[i];
+        }
+    }
+
+    void display()
+    {
+        for(int i=0;i<3;i++)
+        {
+            cout<<arr[i]<<" ";
+        }
+    }
+
+    ~Array()
+    {
+        cout<<"DESTROYED";
+    }
+
+};
+
+int main()
+{    
+    Array arr[3];  
+
+    for(int i=0;i<3;i++)
+    {
+        arr[i].setvalue();
+    }
+
+    Array temp;
+
+    for(int i=0;i<3;i++)
+    {
+        temp=temp+arr[i];
+    }
+    temp.display();
+    
+ 
+
+
+}   

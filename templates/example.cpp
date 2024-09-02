a8d14b6f9fa0d5727f41f47a5b0ba9f10476cf95
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-09-02 11:58:15 +0530

    a

---
+++ templates/example.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+
+class vector
+{
+    public:
+  int *arr;
+  int size;
+  public:
+  vector(int m)
+  {
+    size=m;
+    arr=new int[size];
+
+  }
+  int dotproduct( vector &v)
+  {
+   int d=0;
+   for(int i=0;i<size;i++)
+   {
+    d+=this->arr[i] * arr[i];
+    return d;
+   }
+  }
+};
+int main()
+{
+vector v1(3);
+v1.arr[0]=4;
+v1.arr[1]=5;
+v1.arr[2]=50;
+vector v2(3);
+v1.arr[0]=4;
+v1.arr[1]=1;
+v1.arr[2]=2;
+int a=v1.dotproduct(v2);
+cout<<a<<endl;
+
+}

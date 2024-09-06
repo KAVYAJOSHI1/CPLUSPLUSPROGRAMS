b367048a085a716890557a239ecce79211e1eac8
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-09-06 15:30:39 +0530

    a

--- file handling/outpputfile.cpp
+++ file handling/outpputfile.cpp
@@ -1,13 +1,23 @@
 //To print the contents of the file
-//istream for input
-//ostream for output
+//iftream for input
+//ofstream for output
+
 #include<iostream>
 #include<fstream>
 using namespace std;
 int main()
 {
-    ofstream infile;
+    /*ofstream infile;
     infile.open("ex1.txt",ios::out);
     infile<<"This is india";
-    infile.close();
+    infile.close();*/
+    
+    ifstream file;
+    file.open("ex1.txt");
+    char data[10];
+    file>>data;
+    file.getline( data,10);
+    cout<<data;
+    file.close();
+    return 0;
 }

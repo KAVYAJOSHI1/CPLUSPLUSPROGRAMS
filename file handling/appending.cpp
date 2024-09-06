642b3a77efb31b5550aec81141c17d76ebc3bf75
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-09-06 22:41:04 +0530

    a

---
+++ file handling/appending.cpp
@@ -0,0 +1,19 @@
+//appending to an existing file
+#include<iostream>
+#include<fstream>
+using namespace std;
+
+int main()
+{
+    ofstream file("ex1.txt",ios:app);
+    if(file.is_open())
+    {
+        file<<"Appended another text."<<endl;
+        file.close();
+        cout<<"Appended"<<endl;
+    }
+    else
+    {
+        cout<<"Error opening file"<<endl;
+    }
+}

322f988995812676529eaa5b923e12f3c5f45c5c
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-09-06 22:51:03 +0530

    a

--- file handling/append2file.cpp
+++ file handling/append2file.cpp
@@ -5,7 +5,8 @@ using namespace std;
 
 int main()
 {
-    fstream file1,file2,file3;
+    ifstream file1,file2;
+    ofstream file3;
     file1.open("one.txt",ios::out);
     file2.open("two.txt",ios::out);
     file3.open("three.txt",ios::app);

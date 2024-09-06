93936aaefa4b6b0d73447e443b964057938be8fb
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-09-06 19:43:07 +0530

    a

--- file handling/append2file.cpp
+++ file handling/append2file.cpp
@@ -6,10 +6,38 @@ using namespace std;
 int main()
 {
     fstream file1,file2,file3;
-    file1.open("one.txt",ios::in);
-    file2.open("two.txt",ios::in);
+    file1.open("one.txt",ios::out);
+    file2.open("two.txt",ios::out);
     file3.open("three.txt",ios::app);
     
+    if(file1.is_open())
+    {
+        string line;
+        while(getline(file1,line))
+        {
+            file3<<line<<endl;
+        }
+        file1.close();
+    }
+    else{
+        cout<<"Error opening file";
+    }if(file2.is_open())
+    {
+        string line;
+        while(getline(file2,line))
+        {
+            file3<<line<<endl;
+        }
+        file2.close();
+    }
+    else
+    {
+        cout<<"Error opening file";
+    }
+
+    file3.close();
+    cout<<"Completed merging files"<<endl;
+    return 0;
 
 
 }

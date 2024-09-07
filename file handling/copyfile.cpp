68af1431e117c71e629603fe8a2c85ab72acb2c7
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-09-07 14:44:15 +0530

    a

--- file handling/copyfile.cpp
+++ file handling/copyfile.cpp
@@ -1,10 +1,25 @@
 //TO COPY CONTENTS FROM ONE FILE TO ANOTHER FILE
-#include<iostream>
+#include <iostream>
+#include <fstream>
 using namespace std;
 int main()
 {
-    infile file1="ex1.txt";
-    infile file2="ex2.txt";
-    file1>>file2;
+    ifstream file1("one.txt",ios::in);
+    ofstream file2("two.txt",ios::out);
+    if(file1.is_open() && file2.is_open())
+    {
+        string line;
+        while(getline(file1,line))
+        {
+            file2<<line<<endl;
+        }
+        file1.close();
+        file2.close();
+    }
+    else
+    {
+        cout<<"Error opening file"<<endl;
+    }
     return 0;
 }
+    

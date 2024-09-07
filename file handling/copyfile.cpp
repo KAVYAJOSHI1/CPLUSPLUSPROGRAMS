0d0f892cf399584d4a7c853f148e78318ca3baa5
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-09-07 14:37:07 +0530

    a

---
+++ file handling/copyfile.cpp
@@ -0,0 +1,10 @@
+//TO COPY CONTENTS FROM ONE FILE TO ANOTHER FILE
+#include<iostream>
+using namespace std;
+int main()
+{
+    infile file1="ex1.txt";
+    infile file2="ex2.txt";
+    file1>>file2;
+    return 0;
+}

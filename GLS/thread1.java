6971a063b4fe4c86012e4f9a4d5f19c94311ff92
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-09-20 16:52:12 +0530

    l

---
+++ GLS/thread1.java
@@ -0,0 +1,21 @@
+class MyThread extends Thread{
+
+    public void run(){
+        String name = Thread.currentThread().getName();
+        System.out.println(name + " running");
+        System.out.println(name + " executing");
+        System.out.println(name + " ended");
+    }
+}
+
+public class thread1
+{
+    public static void main(String[] args) {
+        MyThread t1 = new MyThread();
+        MyThread t2 = new MyThread();
+        MyThread t3 = new MyThread();
+        t1.run();
+        t2.run();
+        t3.run();
+    }
+}

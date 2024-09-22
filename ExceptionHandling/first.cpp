daca17df627ac4c495d6e4457de966a3a414e433
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-09-22 09:35:16 +0530

    i

--- ExceptionHandling/first.cpp
+++ ExceptionHandling/first.cpp
@@ -12,7 +12,7 @@ int main()
     try{
         if(den==0)
         {
-            throw "DENOMINATOR CANNOT BE 0";
+            throw runtime_error("DENOMINATOR CANNOT BE 0");
         }
         else
         {
@@ -20,8 +20,8 @@ int main()
            cout<<"RESULT IS: "<<frac;
         }
     }
-    catch(const char* e)
+    catch(const runtime_error& e)
     {
-      cout<<"Error: "<<e;
+      cout<<"Error: "<<e.what();
     }
 }

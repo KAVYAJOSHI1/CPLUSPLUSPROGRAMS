5076dca47ab3aa02be837f1ac8b13c24f8b381d8
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-10-04 11:28:14 +0530

    d

--- TASK 9/1.cpp
+++ TASK 9/1.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main()
 {
     fstream file;
-    file.open("example.txt");
+    file.open("example.txt",ios::out);
     file<<"Open-source software is computer software that is released under a license in which the copyright holder grants users the rights to use,"
 <<"study, change, and distribute the software and its source code to anyone and"
 <<"for any purpose. Open-source software may be developed in a collaborative,"

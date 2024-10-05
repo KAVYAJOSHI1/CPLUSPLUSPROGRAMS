b8ef04a57b25b067f4d0c6ff26ad924808a9801f
KAVYAJOSHI1 <kavyajoshi152005@gmail.com>
2024-10-05 11:29:04 +0530

    m

--- TASK 9/1.cpp
+++ TASK 9/1.cpp
@@ -10,4 +10,50 @@ int main()
 <<"for any purpose. Open-source software may be developed in a collaborative,"
 <<"public manner";
 file.close();
+file.open("example.txt",ios::in);
+file.seekg(50,ios::beg);
+string content;
+while(getline(file,content))
+{
+    cout<<content<<endl;
 }
+file.close();
+cout<<"endl";
+
+file.open("example.txt",ios::in);
+file.seekg(0,ios::beg);
+char buffer[51];
+file.read(buffer,50);
+buffer[50]='\0';
+cout<<buffer<<endl;
+file.close();
+cout<<"endl";
+
+file.open("example.txt",ios::in);
+file.seekg(15,ios::cur);
+
+while(getline(file,content))
+{
+    cout<<content<<endl;
+}
+file.close();
+
+
+
+
+file.open("example.txt",ios::in);
+file.seekg(0,ios::end);
+file.seekg(-20,ios::cur);
+cout<<endl;
+
+
+while(getline(file,content))
+{
+    cout<<content<<endl;
+}
+file.close();
+
+return 0;
+}
+
+
